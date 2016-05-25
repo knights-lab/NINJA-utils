a1dcc2255b428c23a569673faf5de1f7626f708a
Benjamin Hillmann <bhillmann@outlook.com>
2016-05-25 13:57:59 -0500

    Initialized the new repository.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from glob import glob
+import os
+
+__author__ = "Knights Lab"
+__copyright__ = "Copyright (c) 2016--, %s" % __author__
+__credits__ = ["Benjamin Hillmann", "Dan Knights", "Gabe Al-Ghalith", "Tonya Ward", "Pajua Vangay"]
+__email__ = "hillmannben@gmail.com"
+__license__ = "GPL"
+__maintainer__ = "Benjamin Hillmann"
+__version__ = "0.0.1-dev"
+
+long_description = ''
+
+setup(
+    name='ninja_dojo',
+    version=__version__,
+    packages=['ninja_dojo'],
+    url='',
+    license=__license__,
+    author=__author__,
+    author_email=__email__,
+    description='',
+    long_description=long_description,
+    # scripts=glob(os.path.join('scripts', '*py')),
+    keywords='',
+    install_requires=['click', 'networkx']
+)
