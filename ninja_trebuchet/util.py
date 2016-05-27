990a3e1e90c31ad8ec35a81bd6076d1e6d0d38b6
Benjamin Hillmann <bhillmann@outlook.com>
2016-05-27 13:49:55 -0500

    Added the utils package.

--- ninja_trebuchet/util.py
+++ ninja_trebuchet/util.py
@@ -1,2 +1,12 @@
+import urllib.request
+
+
 def reverse_dict(d):
     return {v: k for k, v in d.items()}
+
+
+def download_txt_url(path_to_file, url):
+    with urllib.request.urlopen(url) as stream:
+        with open(path_to_file, 'wb') as outfile:
+            for line in stream:
+                outfile.write(line)
