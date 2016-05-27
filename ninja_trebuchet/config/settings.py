b3d9a36c00b928726ae7d29030d21f369523352f
Benjamin Hillmann <bhillmann@outlook.com>
2016-05-27 15:00:08 -0500

    Added the new settings module.

--- ninja_trebuchet/config/settings.py
+++ ninja_trebuchet/config/settings.py
@@ -1,40 +1,50 @@
 import os
-from collections import defaultdict
 from multiprocessing import cpu_count
 
 import yaml
-from ..path import verify_make_dir
+from ninja_trebuchet.path import verify_make_dir
 
 
 class Settings:
-    def __init__(self, default_settings_dict: dict, config_dir=os.path.join(os.path.expanduser('~'), '.ninja')):
+    def __init__(self, submodule: str, default_settings_factory, config_dir=os.path.join(os.path.expanduser('~'), '.ninja')):
         verify_make_dir(config_dir)
+
+        default_settings_dict = default_settings_factory()
 
         if os.path.exists(os.path.join(config_dir, 'SETTINGS.yaml')):
             with open(os.path.join(config_dir, 'SETTINGS.yaml')) as inf_handle:
-                j = yaml.load(inf_handle)
-                for key in j.keys():
-                    if key in default_settings_dict:
-                        default_settings_dict[key] = j[key]
+                loaded_dict = yaml.load(inf_handle)
+                if submodule in loaded_dict:
+                    sm = loaded_dict[submodule]
+                    for key in default_settings_dict.keys():
+                        if key in sm:
+                            default_settings_dict[key] = sm[key]
+                        else:
+                            if 'dir' in key:
+                                default_settings_dict[key] = os.path.join([self.default_dir]
+                                                                          + default_settings_dict[key])
+                else:
+                    loaded_dict[submodule] = default_settings_dict
+
+                if 'default_dir' in loaded_dict[submodule]:
+                    self.default_dir = loaded_dict[submodule]['default_dir']
+                else:
+                    self.default_dir = os.path.join(config_dir, submodule)
         else:
-            with open(os.path.join(config_dir, 'SETTINGS.yaml'), 'w') as settings_handle:
-                yaml.dump(default_settings_dict, settings_handle)
+            self.default_dir = os.path.join(config_dir, submodule)
+            for name in default_settings_dict:
+                if 'dir' in name:
+                    path = os.path.join([self.default_dir] + default_settings_dict[name])
+                    verify_make_dir(path)
+                    default_settings_dict[name] = path
+            loaded_dict = {submodule: default_settings_dict}
 
-        self.config_dir = config_dir
-        self.get_path = defaultdict(self.path_return)
-
-        for outdir in [item for item in default_settings_dict.keys() if 'dir' in item]:
-            verify_make_dir(default_settings_dict[outdir])
-            self.get_path[outdir] = default_settings_dict[outdir]
+        self.settings = loaded_dict[submodule]
 
         if 'N_jobs' not in default_settings_dict:
             default_settings_dict['N_jobs'] = cpu_count()
 
         self.N_jobs = default_settings_dict['N_jobs']
 
-    def path_return(self, name: str):
-        if 'dir' in name:
-            name = name[name.find('_dir')]
-        path = os.path.join(self.config_dir, name)
-        verify_make_dir(path)
-        return path
+        with open(os.path.join(config_dir, 'SETTINGS.yaml'), 'w') as outf_handle:
+            yaml.dump(loaded_dict, outf_handle)
