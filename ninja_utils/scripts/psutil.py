7f78dda8fa2aba9bbb347a78a47c2c95403a94ef
Benjamin Hillmann <bhillmann@outlook.com>
2016-08-17 15:01:12 -0500

    Made ninja_time.

--- ninja_utils/scripts/psutil.py
+++ ninja_utils/scripts/psutil.py
@@ -3,7 +3,10 @@ from subprocess import Popen, PIPE
 import sys
 
 # This script prints the maximum memory usage in KB for a task
+def psutil():
+    p = Popen(['time', '-f', '%M'] + sys.argv[1:], stderr=PIPE)
+    ru_maxrss = int(p.communicate()[1])
+    print("Maximum rss %d KB" % ru_maxrss)
 
-p = Popen(['time', '-f', '%M'] + sys.argv[1:], stderr=PIPE)
-ru_maxrss = int(p.communicate()[1])
-print("Maximum rss %d KB" % ru_maxrss)
+if __name__ == '__main__':
+    psutil()
