ff72aa02b5ff00c84ae65214e6868260546f19e5
Benjamin Hillmann <bhillmann@outlook.com>
2016-08-24 15:26:48 -0500

    Added the subset fasta call.

--- ninja_utils/scripts/subset_fasta.py
+++ ninja_utils/scripts/subset_fasta.py
@@ -9,9 +9,8 @@ from ninja_utils.parsers import FASTA
 
 def make_arg_parser():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('input', help='If nothing is given, then stdin, else the input file')
+    parser.add_argument('input', required=True, help='If nothing is given, then stdin, else the input file')
     parser.add_argument('-o', '--output', help='If nothing is given, then stdout, else write to file')
-    parser.add_argument('-n', '--number', help='The number of sequences', required=True, type=int)
     parser.add_argument('-k', '--keep', help='The number of sequences to keep', required=True, type=int)
     return parser
 
@@ -26,13 +25,16 @@ def filter_fasta(fasta_gen, total, keep):
 def subset_fasta():
     parser = make_arg_parser()
     args = parser.parse_args()
-    with open(args.input) if args.input else sys.stdin as inf:
-        fasta_gen = FASTA(inf).read()
-        filtered_fasta_gen = filter_fasta(fasta_gen, args.number, args.keep)
+    with open(args.input) as inf:
+        fasta = FASTA(inf)
+        num_reads = sum(1 for i in fasta.read())
+    with open(args.input) as inf:
+        fasta = FASTA(inf)
+        fasta_gen = fasta.read()
+        filtered_fasta_gen = filter_fasta(fasta_gen, num_reads, args.keep)
         with open(args.output, 'w') if args.output else sys.stdout as outf:
             for title, data in filtered_fasta_gen:
-                outf.write('>' + title + os.linesep)
-                outf.write(data + os.linesep)
+                outf.write('>%s\n%s\n' % (title, data))
 
 if __name__ == '__main__':
     subset_fasta()
